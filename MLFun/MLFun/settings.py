6bc5df28e9a6e5c965af6398f6dc3c58675c1c30
ori <benzvi.ori@gmail.com>
2017-02-20 22:13:57 +0200

    Added stack overflow app and a skeleton for the data extraction

--- MLFun/MLFun/settings.py
+++ MLFun/MLFun/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'stack_overflow_center'
 ]
 
 MIDDLEWARE = [
---
+++ MLFun/stack_overflow_center/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class StackOverflowCenterConfig(AppConfig):
+    name = 'stack_overflow_center'
